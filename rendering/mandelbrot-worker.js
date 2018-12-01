665d7f9b00b108aec95ec8ccda5634bd91917b4a
Niels de Jong <niels@nielsdejong.nl>
2018-12-01 14:26:24 +0100

    Added terrain colors and start of framemwork for objects

--- rendering/mandelbrot-worker.js
+++ rendering/mandelbrot-worker.js
@@ -1,7 +1,7 @@
+importScripts( 'perlin/perlin.js');
+noise.seed( 501 );
 
 onmessage = function(e) {
-    importScripts( 'perlin/perlin.js');
-    noise.seed(2);
     var tileCount = 1 << e.data.zoom;
     var yMin = e.data.row / tileCount;
     var yMax = (e.data.row + 1) / tileCount;
@@ -12,24 +12,40 @@ onmessage = function(e) {
      var data = e.data.data = [];
       for (var y = 0, i = 0; y < e.data.size.y; y++) {
         for (var x = 0; x < e.data.size.x; x++) {
-            var value = doNoise(x,y, xMin, yMin, step, e.data.zoom + 8);
-             data[i++] = 256 * (value + 1) / 2.0;
-             data[i++] = 256 * (value + 1) / 2.0;
-             data[i++] = 256 * (value + 1) / 2.0;
-             data[i++] = 255;
-            // data[i++] = (yMin + y * step) * 256.0 + Math.random() * 45;
-            // data[i++] = (xMin + x * step) * 256.0 + Math.random() * 45;
-            // data[i++] = 0;
-            // data[i++] = 255;
+            var value = doNoise(x,y, xMin, yMin, step, 10);
+            value = (value + 1 ) / 2.0;
+            // Value is in a range from -1 to 1.
+
+            if ( value < 0.5 ) {
+                data[i++] = 64 * value;
+                data[i++] = 64 * value;
+                data[i++] = 255 * value;
+                data[i++] = 255;
+            }else if ( value < 0.55) {
+                data[i++] = 200 - 1500 * (value - 0.50);
+                data[i++] = 200 - 1500 * (value - 0.50);
+                data[i++] = 64 - 1500 * (value - 0.50);
+                data[i++] = 255;
+            }else if ( value < 0.9 ){
+                data[i++] = 64 * value;
+                data[i++] = 128 * value;
+                data[i++] = 64 * value;
+                data[i++] = 255;
+            }else{
+                data[i++] = 255 * value;
+                data[i++] = 255 * value;
+                data[i++] = 255 * value;
+                data[i++] = 255;
+            }
         }
       }
-
+    console.log(e);
   postMessage(e.data);
 };
 
 function doNoise(x, y, xMin, yMin, step, maxIterations) {
     var value = 0;
-    for ( var i = 1; i <= maxIterations; i+=2 ){
+    for ( var i = 1; i <= maxIterations; i+=1 ){
         value += noise.simplex2(Math.pow(2, i) *(yMin + y * step), Math.pow(2, i) *(xMin + x * step)) / i;
     }
     return value;
--- rendering/mandelbrot.js
+++ rendering/mandelbrot.js
@@ -1,16 +1,81 @@
+
+noise.seed( 501 );
+
 var po = org.polymaps;
 
 var map = po.map()
     .container(document.getElementById("map").appendChild(po.svg("svg")))
     .zoomRange([0, 20])
     .zoom(1)
-    .center({lat: 0, lon: 0})
+    .center({lat: 0.5, lon: 0.5})
     .add(po.interact())
-    .add(po.hash());
+    .add(po.hash()
+
+);
 
 
 map.add(po.procedural()
-    .worker("mandelbrot-worker.js"));
+    .worker("mandelbrot-worker.js")
+);
 
 map.add(po.compass()
-    .pan("none"));
+    .pan("none")
+);
+
+map.add(po.geoJson()
+    .on("load", load)
+    .clip(false)
+);
+
+
+//var x = document.getElementsByClassName("map")[0];
+
+// var defs = document.createElement("defs");
+// var pattern = document.createElement("pattern");
+// pattern.setAttribute("id", "pattern");
+// pattern.setAttribute("patternUnits", "userSpaceOnUse");
+// pattern.setAttribute("width", "10");
+// pattern.setAttribute("height", "10");
+// defs.appendChild( pattern );
+// x.appendChild( defs );
+// console.log(x);
+
+function load(e) {
+    //console.log(e);
+    //var tile = e.tile, g = tile.element;
+    // var bigness = Math.pow(2, e.tile.zoom);
+    // var smallness = 1.0 / bigness;
+    // for (let x = 0; x < 255; x++) {
+    //     for (let y = 0; y < 255; y++) {
+    //
+    //         //var q = doNoise(x * smallness, y * smallness, tile.row, tile.column, 1000000 * smallness, 1);
+    //         //if(x==0)
+    //         //    console.log(q);
+    //         //console.log(q);
+    //         if ( Math.random() > 0.9999) {
+    //             var point = g.appendChild(po.svg("image"));
+    //             point.setAttribute("width", 0.0125 * bigness);
+    //             point.setAttribute("x", Math.random() * 255);
+    //             point.setAttribute("y", Math.random() * 255);
+    //             point.setAttribute("height", 0.0125 *bigness);
+    //             //var url = "https://images.vexels.com/media/users/3/127601/isolated/lists/4874bc2389e71df4c479ad933b12226a-elliptical-tree-icon.png";
+    //             //var url = "https://cdn0.iconfinder.com/data/icons/simple-flat-colored-trees/100/Tree_green_circle_branches-512.png";
+    //             var url = "https://cdn0.iconfinder.com/data/icons/pixelo/32/tree.png";
+    //             //var url = "https://image.flaticon.com/icons/png/128/72/72223.png";
+    //             point.setAttribute("href", url);
+    //         }
+    //     }
+    // }
+
+    //point.setAttribute("fill", "url(\"https://d1nhio0ox7pgb.cloudfront.net/_img/g_collection_png/standard/64x64/tree.png\")");
+    //point.setAttribute("fill", "red");
+
+}
+
+function doNoise(x, y, xMin, yMin, step, maxIterations) {
+    var value = 0;
+    for ( var i = 1; i <= maxIterations; i+=1 ){
+        value += noise.simplex2(Math.pow(2, i) *(yMin + y * step), Math.pow(2, i) *(xMin + x * step)) / i;
+    }
+    return value;
+}
